065a623902ba438ee851826625a61e7e9b5589be
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-04-07 14:15:31 -0300

    fix: padronizar aspas de string e melhorar a consistência do código em componentes de layout, página e pesquisa de herói

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,37 @@
-import './globals.css'
+import './globals.css';
 
-import type { Metadata } from 'next'
-import React from 'react'
+import type { Metadata } from 'next';
+import React from 'react';
 
-import { Header } from '@/components/header'
-import { inter } from '@/lib/fonts'
+import { Header } from '@/components/header';
+import { inter } from '@/lib/fonts';
 
 export const metadata: Metadata = {
-  title: 'Portal de Serviços | Teresina',
-  description: 'Prefeitura de Teresina',
+  title: 'Portal de Serviços | João Pessoa',
+  description: 'Prefeitura de João Pessoa',
   icons: {
     icon: '/favicon.png',
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="pt">
       <body className={`${inter.className} antialiased`}>
         <Header />
         {children}
+        <footer className="flex items-center p-8 h-10 bg-gray-700 justify-center">
+          <p className="text-xs text-center md:text-sm text-gray-400">
+            © {new Date().getFullYear()} Prefeitura de João Pessoa.Todos os
+            direitos reservados. <br />
+            Desenvolvido por DSF - Inteligência Tributária Municipal.
+          </p>
+        </footer>
       </body>
     </html>
-  )
+  );
 }
