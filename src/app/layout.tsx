1bc42cac8744ccd1b61fd28635c54537862cb83d
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-04-29 15:37:36 -0300

    Atualizar URLs em ServiceCardOnline para usar a variável de ambiente NEXT_PUBLIC_BASE_URL_ONLINE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,8 @@ export const metadata: Metadata = {
   },
 }
 
+const currentYear = new Date().getFullYear()
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,8 +28,8 @@ export default function RootLayout({
         {children}
         <footer className="flex items-center p-10  h-10 bg-gray-700 justify-center">
           <p className="text-xs text-center md:text-sm text-gray-400">
-            © {new Date().getFullYear()} Prefeitura de João Pessoa.Todos os
-            direitos reservados. <br />
+            © {currentYear} Prefeitura de João Pessoa.Todos os direitos
+            reservados. <br />
             Desenvolvido por DSF - Inteligência Tributária Municipal.
           </p>
         </footer>
--- src/components/service-card-online.tsx
+++ src/components/service-card-online.tsx
@@ -21,31 +21,31 @@ const onlineItems = [
     title: 'DTE',
     subtitle: 'Domicílio Tributário Eletrônico',
     icon: <Mail />,
-    href: `${process.env.NEXT_PUBLIC_BASE_URL}/dte/paginas/portal`,
+    href: `${process.env.NEXT_PUBLIC_BASE_URL_ONLINE}/dte/paginas/portal`,
   },
   {
     title: 'NFS-e',
     subtitle: 'Nota Fiscal Eletrônico de Serviços',
     icon: <NotepadText />,
-    href: `${process.env.NEXT_PUBLIC_BASE_URL}/notafiscal/paginas/portal`,
+    href: `${process.env.NEXT_PUBLIC_BASE_URL_ONLINE}/notafiscal/paginas/portal`,
   },
   {
     title: 'NFS-e Avulsa',
     subtitle: 'Nota Fiscal Eletrônico Avulsa',
     icon: <NotepadTextDashedIcon />,
-    href: `${process.env.NEXT_PUBLIC_BASE_URL}/notafiscal/paginas/portal-avulsa`,
+    href: `${process.env.NEXT_PUBLIC_BASE_URL_ONLINE}/notafiscal/paginas/portal-avulsa`,
   },
   {
     title: 'DES-IF',
     subtitle: 'Declaração Eletrônica de Serviços',
     icon: <Landmark />,
-    href: `${process.env.NEXT_PUBLIC_BASE_URL}/desif-web/paginas/portal/index.html`,
+    href: `${process.env.NEXT_PUBLIC_BASE_URL_ONLINE}/desif-web/paginas/portal/index.html`,
   },
   {
     title: 'ITBI-e',
     subtitle: 'Transferências e cessões de imóveis',
     icon: <HousePlus />,
-    href: `${process.env.NEXT_PUBLIC_BASE_URL}/itbi/paginas/portal/index.html`,
+    href: `${process.env.NEXT_PUBLIC_BASE_URL_ONLINE}/itbi/paginas/portal/index.html`,
   },
 ]
 
