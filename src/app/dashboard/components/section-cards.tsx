c87c204912726d4f6e7a4d4730d9beb890189fd8
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-08-29 15:45:37 -0300

    Refatorar o componente SectionCards: atualizar descrições, títulos e ícones dos cartões para maior clareza e relevância

--- src/app/dashboard/components/section-cards.tsx
+++ src/app/dashboard/components/section-cards.tsx
@@ -1,4 +1,13 @@
-import { IconTrendingDown, IconTrendingUp } from '@tabler/icons-react'
+import {
+  IconAlertCircle,
+  IconCar,
+  IconCoin,
+  IconEdit,
+  IconId,
+  IconMap,
+  IconMoneybag,
+  IconPin,
+} from '@tabler/icons-react'
 
 import { Badge } from '@/components/ui/badge'
 import {
@@ -12,89 +21,89 @@ import {
 
 export function SectionCards() {
   return (
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
-      <Card className="@container/card">
+    <div className='*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4'>
+      <Card className='@container/card'>
         <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
+          <CardDescription>IPTU - Valor anual</CardDescription>
+          <CardTitle className='text-2xl font-semibold tabular-nums @[250px]/card:text-3xl'>
+            R$438,69
           </CardTitle>
           <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
+            <Badge variant='outline'>
+              <IconMoneybag />
               +12.5%
             </Badge>
           </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <IconTrendingUp className="size-4" />
+        <CardFooter className='flex-col items-start gap-1.5 text-sm'>
+          <div className='line-clamp-1 flex gap-2 font-medium'>
+            Parcelas pagas <IconCoin className='size-4' />
           </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
+          <div className='text-muted-foreground'>2/10</div>
         </CardFooter>
       </Card>
-      <Card className="@container/card">
+      <Card className='@container/card'>
         <CardHeader>
-          <CardDescription>New Customers</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
+          <CardDescription>Mobiliário</CardDescription>
+          <CardTitle className='text-2xl font-semibold tabular-nums @[250px]/card:text-3xl'>
+            4 Imóveis
           </CardTitle>
           <CardAction>
-            <Badge variant="outline">
-              <IconTrendingDown />
-              -20%
+            <Badge variant='outline'>
+              <IconMap />
+              Ver local
             </Badge>
           </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <IconTrendingDown className="size-4" />
+        <CardFooter className='flex-col items-start gap-1.5 text-sm'>
+          <div className='line-clamp-1 flex gap-2 font-medium'>
+            Uso principal <IconPin className='size-4' />
           </div>
-          <div className="text-muted-foreground">
-            Acquisition needs attention
-          </div>
+          <div className='text-muted-foreground'>Comercial</div>
         </CardFooter>
       </Card>
-      <Card className="@container/card">
+      <Card className='@container/card'>
         <CardHeader>
-          <CardDescription>Active Accounts</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            45,678
+          <CardDescription>Veículos - IPVA</CardDescription>
+          <CardTitle className='text-2xl font-semibold tabular-nums @[250px]/card:text-3xl'>
+            R$145,67
           </CardTitle>
           <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
+            <Badge variant='outline'>
+              <IconCar />
+              4/8
             </Badge>
           </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <IconTrendingUp className="size-4" />
+        <CardFooter className='flex-col items-start gap-1.5 text-sm'>
+          <div className='line-clamp-1 flex gap-2 font-medium'>
+            Documentação
+            <IconId className='size-4' />
           </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
+          <div className='text-muted-foreground'>Licenciamento</div>
         </CardFooter>
       </Card>
-      <Card className="@container/card">
+      <Card className='@container/card'>
         <CardHeader>
-          <CardDescription>Growth Rate</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            4.5%
+          <CardDescription>Notificação</CardDescription>
+          <CardTitle className='text-2xl font-semibold tabular-nums @[250px]/card:text-3xl'>
+            2 alertas
           </CardTitle>
           <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +4.5%
+            <Badge className='bg-yellow-200' variant='outline'>
+              <IconAlertCircle />
+              Atenção
             </Badge>
           </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase <IconTrendingUp className="size-4" />
+        <CardFooter className='flex-col items-start gap-1.5 text-sm'>
+          <div className='line-clamp-1 flex gap-2 font-medium'>
+            Complete informações de cadastro
+            <IconEdit className='size-4' />
           </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
+          <div className='text-muted-foreground'>
+            Atualização Cadastral 2025
+          </div>
         </CardFooter>
       </Card>
     </div>
