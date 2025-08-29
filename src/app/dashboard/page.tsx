5cfdf9fff20cae8e8defeb8be674e26e1d00944b
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-08-29 15:57:01 -0300

    Adicionar tabela de registro de faturas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,30 +1,34 @@
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 
-import { AppSidebar } from "./components/app-sidebar";
-import { SectionCards } from "./components/section-cards";
-import { SiteHeader } from "./components/site-header";
+import { AppSidebar } from './components/app-sidebar'
+import { SectionCards } from './components/section-cards'
+import { SiteHeader } from './components/site-header'
+import { DataTable } from './components/data-table'
 
 export default function Dashboard() {
   return (
     <SidebarProvider
       style={
         {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)"
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
         } as React.CSSProperties
       }
     >
-      <AppSidebar variant="inset" />
+      <AppSidebar variant='inset' />
       <SidebarInset>
         <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <div className='flex flex-1 flex-col'>
+          <div className='@container/main flex flex-1 flex-col gap-2'>
+            <div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'>
               <SectionCards />
+            </div>
+            <div className='px-6'>
+              <DataTable />
             </div>
           </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
-  );
+  )
 }
