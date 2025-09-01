6536cc25db1a73fdb333ab45f5faad2476c01cb0
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-09-01 10:58:08 -0300

    Refatorar o componente LoginForm: melhorar a estrutura e a legibilidade do código

--- src/app/login/components/login-form.tsx
+++ src/app/login/components/login-form.tsx
@@ -11,7 +11,7 @@ import Link from 'next/link'
 
 export function LoginForm() {
   return (
-    <div>
+    <div className='grid grid-cols-2 gap-4'>
       <Card>
         <CardHeader>
           <CardTitle>Entrar em sua conta</CardTitle>
@@ -22,51 +22,68 @@ export function LoginForm() {
         </CardHeader>
         <CardContent>
           <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
+            <div className='flex flex-col gap-6'>
+              <div className='grid gap-3'>
                 <Input
-                  id="email"
-                  type="email"
-                  placeholder="Seu e-mail"
+                  id='email'
+                  type='email'
+                  placeholder='Seu e-mail'
                   required
                 />
               </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
+              <div className='grid gap-3'>
+                <div className='flex items-center'>
                   <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    href='#'
+                    className='ml-auto inline-block text-sm underline-offset-4 hover:underline'
                   >
                     Esqueceu sua senha?
                   </a>
                 </div>
                 <Input
-                  id="password"
-                  type="password"
+                  id='password'
+                  type='password'
                   required
-                  placeholder="Senha"
+                  placeholder='Senha'
                 />
               </div>
-              <div className="flex flex-col gap-3">
+              <div className='flex flex-col gap-3'>
                 <Link
-                  href="/dashboard"
-                  type="submit"
-                  className="w-full border rounded-md bg-primary text-white flex items-center justify-center h-9"
+                  href='/dashboard'
+                  type='submit'
+                  className='w-full border rounded-md bg-primary text-white flex items-center justify-center h-9 text-sm'
                 >
                   Entrar
                 </Link>
-                <Button variant="outline" className="w-full">
+                <Button variant='outline' className='w-full'>
                   Entrar com Google
                 </Button>
               </div>
             </div>
-            <div className="mt-4 text-center text-sm">
+            <div className='mt-4 text-center text-sm'>
               Não tem uma conta?{' '}
-              <a href="#" className="underline underline-offset-4">
+              <a href='#' className='underline underline-offset-4'>
                 Cadastrar
               </a>
             </div>
           </form>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Acesso via Certificado Digital</CardTitle>
+          <CardDescription>
+            Insira seu certificado digital para acessar sua conta.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Link
+            href='/dashboard'
+            type='submit'
+            className='w-full border rounded-md bg-primary text-white flex items-center justify-center h-9 px-4 text-sm'
+          >
+            Entrar com Certificado Digital
+          </Link>
         </CardContent>
       </Card>
     </div>
